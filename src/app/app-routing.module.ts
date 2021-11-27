8bda1c1ad7a0186de4c2f1639b492c0b3fbfcd07
Andrey Higuita <santiagohiguitay@gmail.com>
2021-11-27 12:56:57 -0500

    chore: structure project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './layout/components/content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: ContentComponent,
+    children: [
+      {
+        path:'',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadChildren: () => import('./feature/home/home.module').then(m => m.HomeModule)
+      },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContentComponent } from './components/content/content.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContentComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class LayoutModule { }
