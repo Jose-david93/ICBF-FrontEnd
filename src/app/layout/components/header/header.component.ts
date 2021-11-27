22236a4d35a12b706351b6b7909569a84b53dcb4
Andrey Higuita <santiagohiguitay@gmail.com>
2021-11-27 17:29:23 -0500

    feature: header component design

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,17 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.searchInit();
+  }
+
+  searchInit(): void {
+    $(".search-icon").click(function () {
+      $(this).parent().parent().addClass("active");
+    });
+  
+    $(".search-wrapper .btn-close").click(function () {
+      $(this).parent().removeClass("active");
+    });
   }
 
 }
