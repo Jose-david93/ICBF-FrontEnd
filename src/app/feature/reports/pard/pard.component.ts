45c8fefe16785b843a98053a14bab10b70bac7a1
Mike <mikecardonar@gmail.com>
2021-12-08 11:41:43 -0500

    se agrega reporte pard en bi

---
+++ src/app/feature/reports/pard/pard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pard',
+  templateUrl: './pard.component.html',
+  styleUrls: ['./pard.component.sass']
+})
+export class PardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/feature/reports/reports.module.ts
+++ src/app/feature/reports/reports.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ReportsRoutingModule } from './reports-routing.module';
 import { ControlpanelComponent } from './controlpanel/controlpanel.component';
 import { SurveysourceComponent } from './surveysource/surveysource.component';
+import { PardComponent } from './pard/pard.component';
 
 
 @NgModule({
   declarations: [
     ControlpanelComponent,
-    SurveysourceComponent
+    SurveysourceComponent,
+    PardComponent
   ],
   imports: [
     CommonModule,
